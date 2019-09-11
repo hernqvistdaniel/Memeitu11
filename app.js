92a0b09b706c98e98001b5566a3bb33ab32adde1
Daniel Hernqvist <daniel.hernqvist@chasacademy.se>
2019-09-11 11:48:43 +0200

    install bcrypt and hash passwords to mongodb

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(function(err, req, res, next) {
 
 const mongoURI = 'mongodb+srv://dhernqvist:Lorien1986@memeit-lkmkp.mongodb.net/test?retryWrites=true&w=majority'
 
-mongoose.connect(mongoURI);
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
