15bf010fbcf3bc2e09ea26c6d1057c2302397372
Daniel Hernqvist <daniel.hernqvist@chasacademy.se>
2019-09-11 14:49:38 +0200

    Add RESTroutes for topic-rooms

---
+++ routes/rooms.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('./helpers/auth');
+const Room = require('../models/Room');
+
+// INDEX ROOMS
+router.get('/', (req, res, next) => {
+  // IMPLEMENT
+});
+
+// NEW ROOM FORM
+router.get('/new', auth.requireLogin, (req, res, next) => {
+  // IMPLEMENT
+});
+
+// SHOW SPECIFIC ROOM
+router.get('/:id', auth.requireLogin, (req, res, next) => {
+  // IMPLEMENT
+});
+
+// EDIT ROOM
+router.get('/:id/edit', auth.requireLogin, (req, res, next) => {
+  // IMPLEMENT
+});
+
+// UPDATE ROOM
+router.post('/:id', auth.requireLogin, (req, res, next) => {
+  // IMPLEMENT
+});
+
+// CREATE ROOM
+router.post('/', auth.requireLogin, (req, res, next) => {
+  //IMPLEMENT
+});
