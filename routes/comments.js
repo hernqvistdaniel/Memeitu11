694c33ebbf7f5b514e2ceaf7c47065055174961f
Daniel Hernqvist <daniel.hernqvist@chasacademy.se>
2019-09-12 14:41:46 +0200

    Add functionality to comments-router

--- routes/comments.js
+++ routes/comments.js
@@ -18,4 +18,27 @@ router.get('/new', auth.requireLogin, (req, res, next) => {
   });
 });
 
+router.post('/', auth.requireLogin, (req, res, next) => {
+  Room.findById(req.params.roomId, function(err, room) {
+    if (err) { console.error(err) };
+
+    Post.findById(req.params.postId, function(err, post) {
+      if(err) { console.error(err) };
+      
+      let comment = new Comment(req.body);
+      post.comments.unshift(comment);
+
+      post.save(function(err, post) {
+        if(err) { console.error(err) };
+
+        comment.save(function(err, comment) {
+          if(err) { console.error(err) };
+
+          return res.redirect(`/rooms/${room.id}`);
+        });
+      });
+    });
+  });
+});
+
 module.exports = router;
