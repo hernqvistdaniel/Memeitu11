96edc442ad4c63df9cafe475658b2ebb41f5c2ff
Daniel Hernqvist <daniel.hernqvist@chasacademy.se>
2019-10-03 16:35:25 +0200

    Add nvm config file and docker file for setup with containers

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const mongoose = require('mongoose');
 const Handlebars = require('hbs');
 require('dotenv').config();
 
+// test för docker
+
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const rooms = require('./routes/rooms');
@@ -31,7 +33,7 @@ app.use(
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
